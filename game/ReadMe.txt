3c3cfca5bbad7b6c2c684734fd0359eb5bef3432
Maksym Lunin <maksym.lunin@infopulse.com>
2016-10-31 11:19:54 +0200

    Initial Commit

---
+++ game/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  game
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project game
+  Created by Maksym Lunin on 31/10/16
+  Copyright © 2016 Maksym Lunin
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2016
+  All rights reserved
+  http://embedXcode.weebly.com
+
---
+++ game/game.ino
@@ -0,0 +1,71 @@
+//
+// game
+//
+// Description of the project
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		Maksym Lunin
+// 				Maksym Lunin
+//
+// Date			31/10/16 11:19
+// Version		<#version#>
+//
+// Copyright	© Maksym Lunin, 2016
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+#if defined(WIRING) // Wiring specific
+#include "Wiring.h"
+#elif defined(MAPLE_IDE) // Maple specific
+#include "WProgram.h"
+#elif defined(MPIDE) // chipKIT specific
+#include "WProgram.h"
+#elif defined(DIGISPARK) // Digispark specific
+#include "Arduino.h"
+#elif defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(LITTLEROBOTFRIENDS) // LittleRobotFriends specific
+#include "LRF.h"
+#elif defined(MICRODUINO) // Microduino specific
+#include "Arduino.h"
+#elif defined(SPARK) || defined(PARTICLE) // Particle / Spark specific
+#include "Arduino.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(REDBEARLAB) // RedBearLab specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+
+
+// Prototypes
+
+
+// Define variables and constants
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
